cb0fb16dd9270c734659c4f8681231855120bac7
JonusBurger <j.bergner@live.de>
2025-03-13 20:14:13 +0100

    build basic Info

--- src/htmlHandler.js
+++ src/htmlHandler.js
@@ -2,9 +2,16 @@ export default function buildHTML() {
     const forecastDiv = document.getElementById("forecastDiv");
     const headerSection = document.querySelector(".cityHeader");
     const cellSceleton = document.querySelector(".cell");
+    const currentDataDiv = document.querySelector(".currentDataDiv");
 
     function emptyCells() {
         forecastDiv.replaceChildren();
+    }
+
+    function emptyElement(element) {
+        for (let child of element) {
+            child.innerText = "";
+        }
     }
 
     function buildCell(dayInfo) {
@@ -28,8 +35,21 @@ export default function buildHTML() {
         forecastDiv.appendChild(newCell)
     }
 
-    function buildTodayInfo(currentData) {
-        console.log(currentData);
+    function buildTodayInfo(weatherData) {
+        const basicDataDiv = currentDataDiv.querySelector(".basicDiv");
+        emptyElement(basicDataDiv);
+        buildTodayBasicInfo(basicDataDiv, weatherData);
+    }
+
+    function buildTodayBasicInfo(element, weatherData) {
+        const currentTemp = element.querySelector(".currentTemp");
+        const currentIcon = element.querySelector(".currentIcon");
+        const currentTempMin = element.querySelector(".currentTempMin");
+        const currentTempMax = element.querySelector(".currentTempMax");
+
+        currentTemp.innerText = weatherData.currentWeather.temp;
+        currentTempMin.innerText = weatherData.days[0].tempmin;
+        currentTempMax.innerText = weatherData.days[0].tempmax;
     }
 
     function buildHeader(cityName) {
@@ -44,7 +64,7 @@ export default function buildHTML() {
         buildHeader(weatherData.city);
 
         // build current Info
-        buildTodayInfo(weatherData.currentWeather);
+        buildTodayInfo(weatherData);
         // build loop for days
         for (let day of weatherData.weatherForecast.slice(1,8)) {
             buildCell(day);
